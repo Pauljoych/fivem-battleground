48d639abfb91d270e5f5db4d35c0e85559693246
Pauljoych <luthfiquart@gmail.com>
2023-05-15 13:49:31 +0000

    feat: Add custom zone

--- resources/guard/server/sv_internalCheck.lua
+++ resources/guard/server/sv_internalCheck.lua
@@ -1,6 +1,6 @@
 RegisterNetEvent("core-guard:internalDetected")
 AddEventHandler("core-guard:internalDetected", function()
     local src = source
-    print("Eulan detected")
-    DropPlayer(src, "Eulan detected")
+    print("Eulen detected")
+    DropPlayer(src, "Eulen detected")
 end)
