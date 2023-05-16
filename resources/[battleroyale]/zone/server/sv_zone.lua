444c89bd6b5762e5eaca04997379b213ffdee6be
Pauljoych <luthfiquart@gmail.com>
2023-05-16 19:20:37 +0300

    feat: Server side random seed coords generator

--- resources/[battleroyale]/zone/server/sv_zone.lua
+++ resources/[battleroyale]/zone/server/sv_zone.lua
@@ -1,7 +1,7 @@
 ESX.RegisterCommand('czone', 'admin', function(_, args)
-    TriggerClientEvent('vidigg:customZone', -1, vector3(tonumber(args[1]), tonumber(args[2]), tonumber(args[3])))
-<<<<<<< HEAD
-=======
-    print("Jalan")
->>>>>>> e74b1eb (fix: Patch zone)
+    local randomNumTable
+    for i = 1, 10, 1 do
+        randomNumTable[i] = math.random(-500, 500)
+    end
+    TriggerClientEvent('vidigg:customZone', -1, vector3(tonumber(args[1]), tonumber(args[2]), tonumber(args[3])), randomNumTable)
 end, false)
