4c9bb47b58d280b557478be41142516dc670cdab
Pauljoych <luthfiquart@gmail.com>
2023-05-17 10:32:13 +0000

    fix: Indexing nil value on /czone

--- resources/[battleroyale]/zone/server/sv_zone.lua
+++ resources/[battleroyale]/zone/server/sv_zone.lua
@@ -1,5 +1,5 @@
 ESX.RegisterCommand('czone', 'admin', function(_, args)
-    local randomNumTable
+    local randomNumTable = {}
     for i = 1, 10, 1 do randomNumTable[i] = math.random(-500, 500) end
     TriggerClientEvent('vidigg:customZone', -1, vector3(tonumber(args[1]), tonumber(args[2]), tonumber(args[3])), randomNumTable)
 end, false)
